2dd307ec73b645e15f4f0238d14a2d01fe6ac2cd
vijayTelang <vijaytelang45@gmail.com>
2025-08-23 10:23:55 +0530

    fourth commit

---
+++ cie/cie3.c
@@ -0,0 +1,60 @@
+//modify the prog.  to find the largest prime number less than n.
+//2 conditions:if number is 2 print error msg that there is o prime number less than 2
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <omp.h>
+
+// Function to check if a number is prime
+int isPrime(int num) {
+    if (num <= 1)
+        return 0;
+    if (num == 2)
+        return 1;
+    if (num % 2 == 0)
+        return 0;
+
+    int limit = sqrt(num);
+    for (int i = 3; i <= limit; i += 2) {
+        if (num % i == 0)
+            return 0;
+    }
+    return 1;
+}
+
+int main() {
+    int n;
+    int found = 0;
+    int result = -1;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    if (n <= 2) {
+        printf("Error: There is no prime number less than 2.\n");
+        return 0;
+    }
+
+    // Use OpenMP to parallelize the search
+    #pragma omp parallel for shared(found, result)
+    for (int i = n - 1; i >= 2; --i) {
+        // Use a local flag to prevent race condition
+        if (!found && isPrime(i)) {
+            #pragma omp critical
+            {
+                if (!found) {
+                    result = i;
+                    found = 1;
+                }
+            }
+        }
+    }
+
+    if (found)
+        printf("The largest prime number less than %d is: %d\n", n, result);
+    else
+        printf("No prime number found less than %d.\n", n);
+
+    return 0;
+}
